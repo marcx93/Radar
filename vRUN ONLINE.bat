4357c79e915b330e9a06c7e16f433291cde1b7b7
theRageNT <37319315+theRageNT@users.noreply.github.com>
2018-03-22 20:50:47 -0400

    Update vRUN ONLINE.bat

--- vRUN ONLINE.bat
+++ vRUN ONLINE.bat
@@ -1,2 +1,2 @@
-java -jar target\pubg-radar-1.0-SNAPSHOT-jar-with-dependencies.jar 192.168.1.224 PortFilter 192.168.1.171
+java -jar target\pubg-radar-1.0-SNAPSHOT-jar-with-dependencies.jar [COMPUTER RUNNING RADAR IP] PortFilter [COMPUTER RUNNING PUBG IP]
 pause
